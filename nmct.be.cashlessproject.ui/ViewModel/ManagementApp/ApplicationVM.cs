62cb23103918ec229c3f931960bffc39decedd76
Jonathan Houck <jonathan.houck@student.howest.be>
2014-11-25 16:46:46 +0100

    stukje navigatie

---
+++ nmct.be.cashlessproject.ui/ViewModel/ManagementApp/ApplicationVM.cs
@@ -0,0 +1,55 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace nmct.be.cashlessproject.ui.ViewModel.ManagementApp
+{
+    public class ApplicationVM : ObservableObject
+    {
+        public ApplicationVM()
+        {
+            Pages.Add(new BeheerVM());
+            pages.Add(new ProductenVM());
+            // Add other pages
+
+            CurrentPage = Pages[0];
+        }
+
+        private IPage currentPage;
+        public IPage CurrentPage
+        {
+            get { return currentPage; }
+            set { currentPage = value; OnPropertyChanged("CurrentPage"); }
+        }
+
+        private ObservableCollection<IPage> pages;
+        public ObservableCollection<IPage> Pages
+        {
+            get
+            {
+                if (pages == null)
+                    pages = new ObservableCollection<IPage>();
+                return pages;
+            }
+            set
+            {
+                pages = value; OnPropertyChanged("Pages");
+            }
+        }
+
+        public ICommand ChangePageCommand
+        {
+            get { return new RelayCommand<IPage>(ChangePage); }
+        }
+
+        private void ChangePage(IPage page)
+        {
+            CurrentPage = page;
+        }
+    }
+}
---
+++ nmct.be.cashlessproject.ui/ViewModel/ManagementApp/BeheerVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nmct.be.cashlessproject.ui.ViewModel.ManagementApp
+{
+    class BeheerVM : ObservableObject, IPage
+    {
+        public string Name
+        {
+            get { return "Beheer"; }
+        }
+    }
+}
